3121426020642aae3a47e350a3f13e466547da14
Tom Robertshaw <me@tomrobertshaw.net>
2015-08-29 17:51:24 +0100

    Typo in resource collection class name.

--- src/app/code/community/Meanbee/PersonalisedContent/Model/Resource/Customer/Categories/Collection.php
+++ src/app/code/community/Meanbee/PersonalisedContent/Model/Resource/Customer/Categories/Collection.php
@@ -1,6 +1,6 @@
 <?php
 
-class Meanbee_PersonalisedContent_Model_Resouce_Customer_Categories_Collection extends Mage_Core_Model_Resource_Db_Collection_Abstract
+class Meanbee_PersonalisedContent_Model_Resource_Customer_Categories_Collection extends Mage_Core_Model_Resource_Db_Collection_Abstract
 {
     /**
      * Resource collection initialization
