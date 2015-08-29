d89ff7d3c25f605023e4e8e581f65d66e05bb01d
Tom Robertshaw <me@tomrobertshaw.net>
2015-08-29 16:03:08 +0100

    Remove outdated comments.

--- src/app/code/community/Meanbee/PersonalisedContent/Model/Customer/Categories.php
+++ src/app/code/community/Meanbee/PersonalisedContent/Model/Customer/Categories.php
@@ -1,16 +1,5 @@
 <?php
-/**
- * RapidCampaign
- *
- * @category    RapidCampaign
- * @package     RapidCampaign_Promotions
- * @copyright   Copyright (c) 2015 RapidCampaign (http://rapidcampaign.com)
- */
 
-/**
- * @method getExpireTime()
- * @method setExpireTime($datetime)
- */
 class Meanbee_PersonalisedContent_Model_Customer_Categories extends Mage_Core_Model_Abstract
 {
     /**
--- src/app/code/community/Meanbee/PersonalisedContent/Model/Resource/Customer/Categories.php
+++ src/app/code/community/Meanbee/PersonalisedContent/Model/Resource/Customer/Categories.php
@@ -1,11 +1,5 @@
 <?php
-/**
- * RapidCampaign
- *
- * @category    RapidCampaign
- * @package     RapidCampaign_Promotions
- * @copyright   Copyright (c) 2015 RapidCampaign (http://rapidcampaign.com)
- */
+
 class Meanbee_PersonalisedContent_Model_Resouce_Customer_Categories extends Mage_Core_Model_Resource_Db_Abstract
 {
     /**
