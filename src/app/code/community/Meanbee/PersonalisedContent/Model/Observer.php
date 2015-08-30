25320ba5f6f724bd21f664c878ed67c34c7932cd
Tom Robertshaw <me@tomrobertshaw.net>
2015-08-30 08:50:27 +0100

    Check for enabled when running cron.

--- src/app/code/community/Meanbee/PersonalisedContent/Model/Observer.php
+++ src/app/code/community/Meanbee/PersonalisedContent/Model/Observer.php
@@ -8,6 +8,13 @@ class Meanbee_PersonalisedContent_Model_Observer
      */
     public function reindexCustomerCategories()
     {
+        /** @var Meanbee_PersonalisedContent_Helper_Config $config */
+        $config = Mage::helper('meanbee_personalisedcontent/config');
+
+        if (!$config->isEnabled()) {
+            return $this;
+        }
+
         /** @var Meanbee_PersonalisedContent_Helper_Customer_Categories $helper */
         $helper = Mage::helper('meanbee_personalisedcontent/customer_categories');
         $helper->reindexCustomerCategories();
